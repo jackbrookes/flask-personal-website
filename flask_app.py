9d5081f2a19ac7ed07db21808c149d9b410afe67
Jack Brookes <Jack Brookes>
2017-05-04 23:42:01 +0100

    deleted old files

--- flask_app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import datetime
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    curr_time = datetime.datetime.now()
-    return 'Hello, world! <br>{}'.format(curr_time)
